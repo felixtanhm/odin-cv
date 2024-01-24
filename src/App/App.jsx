8d7d8c2bf3e1ae485251077cae7203f595ae1ddf
felixtanhm <felixtanhm@gmail.com>
2024-01-24 13:13:32 +0800

    Fixed Work & Experience form crashing without Profile

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -55,7 +55,7 @@ function App() {
               toggleForm={toggleForms}
             />
           )}
-          {showForm.formType !== "profile" && (
+          {showForm.formType !== "profile" && profile !== null && (
             <FormItem
               formItem={profile}
               formType="profile"
